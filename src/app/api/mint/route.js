40d61ab704537643e3062a437cbe0fce20a0c379
Daniel Fuyà Espinar <danifuya@Daniels-MacBook-Air.local>
2024-01-26 14:40:49 +0100

    post request

--- src/app/api/mint/route.js
+++ src/app/api/mint/route.js
@@ -101,8 +101,8 @@ function getRandomBigInt(max) {
     BigInt(max)
   );
 }
-export async function GET(req) {
-  if (req.method === "GET") {
+export async function POST(req) {
+  if (req.method === "POST") {
     try {
       // Parse request body
       const body = req.body;
